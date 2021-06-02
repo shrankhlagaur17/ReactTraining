29ebe80ab93a711177200507b7cf3acee652f558
shrankhlagaur17 <shrankhla.gaur@appinventiv.com>
2021-06-02 10:48:24 +0530

    module Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React from 'react'
+import React from 'react';
 import Home from 'module/home';
 
 class App extends React.Component {
-  render(){
-    return(
-      <div>
-      <Home/>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<Home />
+			</div>
+		);
+	}
 }
-  
-
 
 export default App;
--- src/module/home/Index.js
+++ src/module/home/Index.js
@@ -1,18 +1,15 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
+import '.index/css';
 
 class Home extends Component {
-    render(){
-        return(
-            <div>
-            <h1>{'hello'}</h1>
-            </div>
-        )
-    }
-	
+	render() {
+		return (
+			<div className={'homeContainer'}>
+				<h1 className={'h1tag'}>{'hello'}</h1>
+				<h2 id={'h2tag'}>{'hello'}</h2>
+			</div>
+		);
+	}
 }
 
 export default Home;
-
-
-
-
